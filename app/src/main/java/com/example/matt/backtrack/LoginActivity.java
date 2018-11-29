070959998f8ba626f375b2a9f95f7022e34e8145
Kumar Vaibhav <k.vaibhav2908@gmail.com>
2018-11-28 23:30:47 -0800

    working_Create_Group

--- app/src/main/java/com/example/matt/backtrack/LoginActivity.java
+++ app/src/main/java/com/example/matt/backtrack/LoginActivity.java
@@ -235,7 +235,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         if (user == null) {
             // There was an error; don't attempt login and focus the first
             // form field with an error.
-            focusView.requestFocus();
+//            focusView.requestFocus();
+            showProgress(true);
+            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask.execute((Void) null);
+            signIn(email, password);
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
